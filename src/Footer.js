1b3fcfbb331eab20e150ddf2459b9fb3a374100c
avrorestina <40735232+avrorestina@users.noreply.github.com>
2020-01-08 23:11:41 +0800

    fixed it

--- src/Footer.js
+++ src/Footer.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import "./styles.css";
 import { SocialIcon } from "react-social-icons";
@@ -22,22 +21,6 @@ export default function Footer() {
         This website is created by me, Reemays. If you like my work and would
         like to support me or hiring me for jobs, please visit one of my social
         media or go "link here later" for contact.
-      </p>
-    </div>
-  );
-}
-import React from "react";
-import "./styles.css";
-
-export default function Footer() {
-  return (
-    <div className="Footer">
-      <p className="FooterItems">
-        This website is created by me, Reemays. If you like my work and would
-        like to support me or hiring me for jobs, please visit one of my social
-        media or go "link here later" for contact;
-        This website is created by me, Reemays. Reach me through my main website
-        at "link here" for more info regarding hiring me for projects.
       </p>
     </div>
   );
