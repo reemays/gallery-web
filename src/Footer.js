027446cd86e29f189e2cafd07b856ee5d6a21f4f
avrorestina <40735232+avrorestina@users.noreply.github.com>
2020-01-08 22:40:11 +0800

    Changing little thing

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,10 @@ import "./styles.css";
 export default function Footer() {
   return (
     <div className="Footer">
-      <p className="FooterItems">Footer lorem ipsum for footer dolor si amet</p>
+      <p className="FooterItems">
+        This website is created by me, Reemays. Reach me through my main website
+        at "link here" for more info regarding hiring me for projects.
+      </p>
     </div>
   );
 }
