633c7bccc7daa1559efdbac1411b987861f286db
avrorestina <40735232+avrorestina@users.noreply.github.com>
2020-01-08 22:45:49 +0800

    footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,11 @@ import "./styles.css";
 export default function Footer() {
   return (
     <div className="Footer">
-      <p className="FooterItems">Footer lorem ipsum for footer dolor si amet</p>
+      <p className="FooterItems">
+        This website is created by me, Reemays. If you like my work and would
+        like to support me or hiring me for jobs, please visit one of my social
+        media or go "link here later" for contact;
+      </p>
     </div>
   );
 }
