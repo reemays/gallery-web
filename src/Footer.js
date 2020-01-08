2258a0aebfc61e7d554fef8cad2efe9ee62a38fc
avrorestina <40735232+avrorestina@users.noreply.github.com>
2020-01-08 23:06:03 +0800

    social and footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,13 +1,26 @@
 import React from "react";
 import "./styles.css";
+import { SocialIcon } from "react-social-icons";
 
 export default function Footer() {
   return (
     <div className="Footer">
+      <SocialIcon
+        url="https://github.com/avrorestina"
+        className="FooterIcons"
+      />
+      <SocialIcon
+        url="https://www.linkedin.com/in/mohammad-syameer-imran-aa5904171/"
+        className="FooterIcons"
+      />
+      <SocialIcon
+        url="https://www.instagram.com/syameer.imran/"
+        className="FooterIcons"
+      />
       <p className="FooterItems">
         This website is created by me, Reemays. If you like my work and would
         like to support me or hiring me for jobs, please visit one of my social
-        media or go "link here later" for contact;
+        media or go "link here later" for contact.
       </p>
     </div>
   );
